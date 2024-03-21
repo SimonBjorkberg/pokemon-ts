1527e45236078364147a3e1b29a3dd10f03a236e
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-03-21 08:17:40 +0100

    conditional rendering of the selectedcontainer

--- src/app/components/SelectedContainer.tsx
+++ src/app/components/SelectedContainer.tsx
@@ -1,9 +1,15 @@
 export default function SelectedContainer({ selectedPokemon }: any) {
 
     console.log(selectedPokemon)
+
     return (
-        <main className="border h-fit w-1/2">
-            {selectedPokemon.name}
-        </main>
+        <>
+            <main className="w-1/2 h-full flex">
+                <div className={`border-2 h-fit mansalva-regular flex flex-col w-fit text-center p-8 bg-neutral-900 border-orange-300 mx-auto my-auto rounded-md ${selectedPokemon.name ? "flex" : "hidden"}`}>
+                    <img className="w-52" src={selectedPokemon.sprites?.front_default} alt="" />
+                    <h1 className="text-4xl">{selectedPokemon.name}</h1>
+                </div>
+            </main>
+        </>
     )
 }
